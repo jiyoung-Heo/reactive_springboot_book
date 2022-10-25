97322aa3c15ad7abf1df0ed9bd51f53a4bb44131
Nylah <jiyoungheo.dev@gmail.com>
2022-10-25 16:03:39 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# reactive_springboot_book
+리액트부트 실전활용 마스터 책 공부
---
+++ src/main/java/com/springboot/reactive/ReactiveSpringbootBookApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringbootBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringbootBookApplication.class, args);
+	}
+
+}
